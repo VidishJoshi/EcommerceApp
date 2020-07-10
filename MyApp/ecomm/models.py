4fda205996377837377d82e9777b2eea6bee4b1f
VidishJoshi <vidish2707@gmail.com>
2020-07-10 12:13:24 +0530

    First commit

---
+++ MyApp/ecomm/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class ProductInformation(models.Model):
+    name = models.CharField(max_length = 32)
+    price = models.FloatField()
+    category = models.CharField(max_length = 32)
+    company = models.CharField(max_length = 32)
+    quantity = models.IntegerField()
+    description = models.TextField(blank = True)
+    
