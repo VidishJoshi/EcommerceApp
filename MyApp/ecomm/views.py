525cd265748da587fd88dbd14a5f193c94429bdb
VidishJoshi <vidish2707@gmail.com>
2020-07-18 20:53:14 +0530

    Added more_info and buy_now

--- MyApp/ecomm/views.py
+++ MyApp/ecomm/views.py
@@ -15,10 +15,32 @@ def view_info(request):
     value = request.GET.get('category')
     content = ProductInformation.objects.filter(category=value).values_list('id', 'name', 'price')
     return render(request, 'view_info.html', {
-        'content' : content
+        'content' : content,
     })
 
 def more_info(request):
     id_from_url = request.GET.get('id')
-    content = ProductInformation.objects.filter(id = id_from_url).values_list()
-    return HttpResponse(content)
+    content = ProductInformation.objects.filter(id = id_from_url).values()
+    return render(request, 'more_info.html', {
+        'content': content[0],
+    })
+
+def buy_now(request):
+
+    if request.method == 'GET':
+        id_from_url = request.GET.get('id2')
+        content = ProductInformation.objects.filter(id=id_from_url).values()
+        content = content[0]
+
+        name = content['name']
+        price = content['price']
+        quantity = content['quantity']
+
+        return render(request, 'buy_now.html', {
+            'name': name,
+            'price': price,
+            'quantity': quantity,
+        })
+    
+    elif request.method == 'POST':
+        return HttpResponse("Works")
